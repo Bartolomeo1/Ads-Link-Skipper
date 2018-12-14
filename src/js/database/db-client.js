9d547a8a0b499d71a093829628b7795529f61083
Nurio Noroty <xxnurioxx@gmail.com>
2018-12-14 10:54:14 +0100

    Started Universal shortener skiper script
    Universal link shortener skiper over database. Users send the shortener result and then all users can skip directly to them.

---
+++ src/js/database/db-client.js
@@ -0,0 +1,48 @@
+/** 
+
+[Ads link skiper] by xXNurioXx
+https://chrome.google.com/webstore/detail/ads-link-skiper/bkpeohkfimdfogdnpcnokjkbpankkmil
+
+Universal link shortener skiper over database. Users send the shortener result and then all users can skip directly to them.
+
+**/
+
+class DBConnection {
+
+    _constructor(){
+        // TODO WSS Connection
+    }
+
+    static downloadDB(){
+        // TODO get complete shortener=>result database
+    }
+
+    static obtainData(shortener){
+        // TODO get specific shortener=>result
+    }
+
+    static sendData(shortener, result){
+        // TODO send local results to an a specific shortener
+    }
+
+}
+
+class shortenerMap {
+
+    static getResultsOf(shortener){
+        // TODO
+    }
+
+    static getMoreAcurratedResultOf(shortener){
+        // TODO
+    }
+
+    static setResultsOf(shortener){
+        // TODO
+    }
+
+    static addResultOf(shortener){
+        // TODO
+    }
+
+}
